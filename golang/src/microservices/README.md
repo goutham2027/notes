279a596ed61df86fd66e591ce7180bcc82b14f5f
Goutham Pilla <goutham2027@gmail.com>
2023-02-14 22:48:35 +0530

    use encode instead of marshal

--- golang/src/microservices/README.md
+++ golang/src/microservices/README.md
@@ -3,3 +3,12 @@ https://www.youtube.com/watch?v=VzBGi_n65iU&ab_channel=NicJackson
 REST - JSON over HTTP
 
 struct tags - annotations to the fields
+
+jq - command line json processor
+https://stedolan.github.io/jq/
+
+marshal vs encode
+encode - directly writes to IO
+marshal - writes to memory first
+
+curl localhost:9090 -X DELETE -v | jq
--- golang/src/microservices/handlers/products.go
+++ golang/src/microservices/handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"microservices/data"
 	"net/http"
@@ -15,11 +14,20 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// handle update
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-	rw.Write(d)
 }

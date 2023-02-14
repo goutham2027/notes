8b19f083fde3503c88c284505030620a97c7afe0
Goutham Pilla <goutham2027@gmail.com>
2023-02-14 23:29:18 +0530

    put, post method implementation

--- golang/src/microservices/handlers/products.go
+++ golang/src/microservices/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"microservices/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -20,6 +22,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect the id in the uri
+		re := regexp.MustCompile(`/([0-9]+)`)
+		g := re.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.l.Println(id)
+
+	}
+
 	// handle update
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
@@ -30,4 +56,21 @@ func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle post")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+	}
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request) {
+
 }

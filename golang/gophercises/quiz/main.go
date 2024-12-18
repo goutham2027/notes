bcb38eadd4c4644c75e01adcc55ee3fe2bc81eb8
Goutham Pilla <goutham2027@gmail.com>
2024-12-19 00:16:56 +0530

    gophercises: ex-1

--- golang/gophercises/quiz/main.go
+++ golang/gophercises/quiz/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	var answer string
+	var csvFilePath string
+	var duration int64
 	right := 0
 
-	f, err := os.Open("problems.csv")
+	fmt.Println("Welcome to the quiz game")
+
+	flag.StringVar(&csvFilePath, "csv-file", "problems.csv", "problems csv file path")
+	flag.Int64Var(&duration, "duration", 3, "Duration of the quiz")
+	flag.Parse()
+
+	timer := time.NewTimer(time.Duration(duration) * time.Second)
+
+	f, err := os.Open(csvFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,17 +31,29 @@ func main() {
 
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, record := range records {
-		fmt.Printf("%s \n", record[0])
-		fmt.Scanln(&answer)
-		if answer == record[1] {
-			right += 1
+	ch := make(chan bool)
+
+	go func(ch chan bool) {
+		for idx, record := range records {
+			fmt.Printf("Problem #%d: %s = ", idx+1, record[0])
+			fmt.Scan(&answer)
+			if answer == record[1] {
+				right += 1
+			}
 		}
+		ch <- timer.Stop()
+	}(ch)
+
+	select {
+	case <-ch:
+		fmt.Println("Exam over before time is up")
+	case <-timer.C:
+		fmt.Println("Time is up")
 	}
+
 	fmt.Printf("Total right answers: %d/%d", right, len(records))
 }

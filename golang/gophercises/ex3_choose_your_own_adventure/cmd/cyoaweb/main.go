5a5d9d79b86993657897b2bc97682715d32fe25c
Goutham Pilla <goutham2027@gmail.com>
2025-01-20 11:18:43 +0530

    gophercises ex3

--- golang/gophercises/ex3_choose_your_own_adventure/cmd/cyoaweb/main.go
+++ golang/gophercises/ex3_choose_your_own_adventure/cmd/cyoaweb/main.go
@@ -4,11 +4,15 @@ import (
 	"cyoa"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the cyoa web app")
 	filename := flag.String("file", "stories.json", "json file with story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s. \n", *filename)
@@ -24,6 +28,50 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", arcs)
+	// fmt.Printf("%+v\n", arcs)
 	// fmt.Println(arcs["debate"].ArcSuggestions[0].ArcName)
+
+	// tpl := template.Must(template.New("").Parse("Hello"))
+	// h := cyoa.NewHandler(arcs, cyoa.WithTemplate(tpl))
+
+	tpl := template.Must(template.New("").Parse(storyTpl))
+
+	h := cyoa.NewHandler(arcs,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
+	fmt.Printf("Starting the server on port %d\n", *port)
+	addr := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var storyTpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Choose your own adventure</title>
+  </head>
+  <body>
+    <h1>"{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .ArcSuggestions}}
+    <li><a href="/story/{{.ArcName}}"> {{.SuggestionText}}</a></li>
+    {{end}}
+  </ul>
+    </body>
+</html>`
--- golang/gophercises/ex3_choose_your_own_adventure/story.go
+++ golang/gophercises/ex3_choose_your_own_adventure/story.go
@@ -2,8 +2,37 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"log"
+	"net/http"
+	"strings"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Choose your own adventure</title>
+  </head>
+  <body>
+    <h1>"{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .ArcSuggestions}}
+    <li><a href="/{{.ArcName}}"> {{.SuggestionText}}</a></li>
+    {{end}}
+  </ul>
+    </body>
+</html>`
 
 type Story map[string]Chapter
 
@@ -26,4 +55,55 @@ func JsonStory(r io.Reader) (Story, error) {
 		return nil, err
 	}
 	return arcs, nil
+}
+
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
+}
+
+type handler struct {
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func defaultPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
+
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "Chapter not found", http.StatusNotFound)
 }

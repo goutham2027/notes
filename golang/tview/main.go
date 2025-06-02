cf763f2ef197df31694f4e6eebf5ce936d70beaa
Goutham Pilla <goutham2027@gmail.com>
2025-06-02 16:45:28 +0530

    intercept keys

--- golang/tview/main.go
+++ golang/tview/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
 	app := tview.NewApplication()
 
+	// Define colors for focused and unfocused states
+	focusedBorderColor := tcell.ColorBlue
+	focusedTitleColor := tcell.ColorRed
+	unfocusedBorderColor := tcell.ColorWhite
+	unfocusedTitleColor := tcell.ColorWhite
+
 	toolWindow := tview.NewTextView().SetText("Select a tool to see the output")
 	toolWindow.SetBorder(true).SetTitle("Tool Window")
+	toolWindow.SetBorderColor(unfocusedBorderColor).SetTitleColor(unfocusedTitleColor)
 
 	// display tools in the tools box
 	tools := tview.NewList()
@@ -19,12 +27,44 @@ func main() {
 		toolWindow.SetText("Hello World 2")
 	})
 	tools.SetBorder(true).SetTitle("Tools")
+	tools.SetBorderColor(focusedBorderColor).SetTitleColor(focusedTitleColor) // Start focused
 
 	options := tview.NewBox().SetBorder(true).SetTitle("Options")
+	options.SetBorderColor(unfocusedBorderColor).SetTitleColor(unfocusedTitleColor)
 
-	// when I click on a tool, I want to display the output in the tool window
-	tools.SetChangedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+	// Function to update focus colors
+	updateFocusColors := func(focused tview.Primitive) {
+		// Reset all to unfocused colors
+		tools.SetBorderColor(unfocusedBorderColor).SetTitleColor(unfocusedTitleColor)
+		toolWindow.SetBorderColor(unfocusedBorderColor).SetTitleColor(unfocusedTitleColor)
+		options.SetBorderColor(unfocusedBorderColor).SetTitleColor(unfocusedTitleColor)
+
+		// Set focused component colors
+		switch focused {
+		case tools:
+			tools.SetBorderColor(focusedBorderColor).SetTitleColor(focusedTitleColor)
+		case toolWindow:
+			toolWindow.SetBorderColor(focusedBorderColor).SetTitleColor(focusedTitleColor)
+		case options:
+			options.SetBorderColor(focusedBorderColor).SetTitleColor(focusedTitleColor)
+		}
+	}
+
+	// when I select a tool by pressing enter, I want to display the output in the tool window
+	// change the focus to the tool window
+	tools.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		app.SetFocus(toolWindow)
+		updateFocusColors(toolWindow)
 		toolWindow.SetText("you selected " + mainText)
+	})
+
+	// when escape is pressed, focus on the tools box
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEscape {
+			app.SetFocus(tools)
+			updateFocusColors(tools)
+		}
+		return event
 	})
 
 	flex := tview.NewFlex()
